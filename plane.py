4a119ccd429c6887ebc974216722e0cfff16d516
Wo0kie3 <44375336+Wo0kie3@users.noreply.github.com>
2020-01-04 17:12:40 +0100

    Update plane.py

--- plane.py
+++ plane.py
@@ -13,12 +13,28 @@ def baggage_normal():
 
 class PassengerAgent(Agent):
     """ An agent with a fixed seat assigned """
-    def __init__(self, unique_id, model, seat_pos):
+    def __init__(self, unique_id, model, seat_pos, group):
         super().__init__(unique_id, model)
         self.seat_pos = seat_pos
         self.baggage = baggage_normal()
-
+        self.x = seat_pos[0]
+        self.y = seat_pos[1]
+        self.group = group
+        self.my_x = 0
+        self.my_y = 3
     def step(self):
+        if self.x != self.seat_pos:
+            self.move()
+        else:
+            #self.store_luggage()
+            pass
+
+    def move(self):
+        self.model.grid.move_agent(self, (self.x + 1, self.y))
+        self.x += 1
+
+    def store_luggage(self):
+        #storing luggage and stoping queue
         pass
 
     def __str__(self):
@@ -31,10 +47,19 @@ class PlaneModel(Model):
         self.grid = MultiGrid(21, 7, False)
         self.running = True
         self.method = method
+        self.entry_free = True
+        self.plane_queue = QueueActivation(self)
 
         # Create agents and splitting them into separate boarding groups accordingly to a given method
         self.boarding_groups = []
         self.method(self)
 
+
     def step(self):
-        pass
+        self.plane_queue.step()
+        self.entry_free = True
+        if self.entry_free:
+            a = self.boarding_groups.pop()
+            self.plane_queue.add(a)
+            self.grid.place_agent(a, (0, 3))
+            self.entry_free = False
