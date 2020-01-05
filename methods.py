6f2451dc946be97d5a6219a22333fd57d3a0b1ec
Wo0kie3 <44375336+Wo0kie3@users.noreply.github.com>
2020-01-05 13:58:43 +0100

    Update methods.py

--- methods.py
+++ methods.py
@@ -13,12 +13,89 @@ def random(model):
     model.boarding_queue.extend(group)
 
 
-def back_to_front_gr(model):
-    pass
-
-
 def front_to_back_gr(model):
-    pass
+    final_group = []
+    id = 1
+    sub_group = []
+    for x in range(19, 15, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(15, 11, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 2)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(11, 7, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(7, 3, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    model.boarding_queue.extend(final_group)
+
+
+def back_to_front_gr(model):
+    final_group = []
+    id = 1
+    sub_group = []
+    for x in range(6, 2, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(10, 6, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(14, 10, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 2)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+    sub_group = []
+    for x in range(18, 14, -1):
+        for y in (0, 1, 2, 4, 5, 6):
+            agent = PassengerAgent(id, model, (x, y), 1)
+            id += 1
+            sub_group.append(agent)
+    model.random.shuffle(sub_group)
+    for a in sub_group:
+        final_group.append(a)
+
+    model.boarding_queue.extend(final_group)
 
 
 def back_to_front(model):
